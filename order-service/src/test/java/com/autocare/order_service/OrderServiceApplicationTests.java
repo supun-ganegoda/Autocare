ed078dba16bd31f06f291618e48776e52341e788
supun-ganegoda <supung@syntaxgenie.com>
2024-11-25 16:26:02 +0530

    Integration test for order placement

--- order-service/src/test/java/com/autocare/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/autocare/order_service/OrderServiceApplicationTests.java
@@ -1,15 +1,58 @@
 package com.autocare.order_service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+
+	@LocalServerPort
+	private Integer port;
+
+	static {
+		mySQLContainer.start();
+	}
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	private static final String requestBody = """
+			{
+			   "skuCode":"Head lights",
+			   "price":15000,
+			   "quantity":1
+			 }
+				""";
+
 
 	@Test
-	void contextLoads() {
+	void shouldCreateOrder() {
+		String response = RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+		assertThat(response, Matchers.equalTo("order placement successful"));
 	}
 
 }
