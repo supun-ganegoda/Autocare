9e140f02663aeed31b001022aaaa06c4200328de
supun-ganegoda <supung@syntaxgenie.com>
2024-11-25 16:03:15 +0530

    Flyway database migration file added

--- order-service/src/main/java/com/autocare/order_service/model/Order.java
+++ order-service/src/main/java/com/autocare/order_service/model/Order.java
@@ -6,6 +6,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/*
+since the hibernate ddl-auto is set to none, hibernate not going to make tables automatically,
+instead we need to create the database using the flyway migration, for that the SQL code should enter in file
+which is located inside the resources db.migration
+the format of the file name should V<number>__<name>.sql
+let's say we want to add an another column to the table, that code can be written in another separate migration file
+which is running automatically in application starts
+*/
+
 @Entity
 @Table(name = "t_orders")
 @Getter
