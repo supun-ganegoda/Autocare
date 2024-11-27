3114fea3abe6845daa7ba3f223dc0d687918d0f3
supun-ganegoda <supung@syntaxgenie.com>
2024-11-27 16:09:12 +0530

    Circuit breaker is added to the order service inventory call

--- order-service/src/main/java/com/autocare/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/autocare/order_service/client/InventoryClient.java
@@ -1,10 +1,21 @@
 package com.autocare.order_service.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
-
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean checkInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable){
+        log.info("Cannot get inventory item {} because {}",skuCode, throwable.getMessage());
+        return false;
+    }
 }
